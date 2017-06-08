99b70931eb705ed3a808a3a27d6d4a054bfcaa45
leotgordon <leotgordon@gmail.com>
2017-06-08 11:12:42 -0700

    13

--- schedule.js
+++ schedule.js
@@ -37,6 +37,6 @@ function enter() {
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
-    document.getElementById(gameNumber).style.display = "block";
+    document.getElementById(gameNumber).style.display = "inline";
     gameNumber += 1;
 }
